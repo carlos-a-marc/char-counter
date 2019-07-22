749d73a891973a0caa1e3cbd284fc8ab55c45285
Carlos Alberto <carlosealberto@hotmail.com.br>
2019-07-22 12:27:20 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: 0,
+      text: []
+    };
+  }
+
+  counter() {
+    let text = document.getElementById('text').value;
+    this.setState({
+      characters: text.length,
+      text
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Character Counter</h1>
+        <h3>Put your text in text area</h3>
+        <textarea className="text-area" id="text" onKeyUp={() => this.counter()}></textarea>
+        <p className="number-char">Characters: {this.state.characters}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
